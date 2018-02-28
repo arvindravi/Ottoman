661cda26357c4016a649aeeeda56b79529defe47
Zachary Borrelli <zac@theappbusiness.com>
2018-02-28 15:24:11 +0000

    Added file for base accessibility control

---
+++ XCUIApplication+AccessibilityControls.swift
@@ -0,0 +1,14 @@
+//
+//  XCUIApplication+AccessibilityControls.swift
+//  Pods-TABTestKit_ExampleUITests
+//
+//  Created by Zachary Borrelli on 29/01/2018.
+//
+
+import XCTest
+
+public extension XCUIApplication {
+  func changeFontSize() {
+    
+  }
+}
